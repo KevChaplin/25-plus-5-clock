e5ffe78c46077af59e5dd65c9bac0831821d6b53
KevChaplin <kevchaplin44@gmail.com>
2021-07-09 18:52:42 +0900

    basic countdown functional

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap'
 import CountdownClock from './components/CountdownClock'
@@ -10,12 +10,21 @@ function App() {
 
   const [session, setSession] = useState(25);
   const [sbreak, setSbreak] = useState(5);
+  const [active, setActive] = useState(false);
+  const [time, setTime] = useState(1500);
+
+  useEffect(() => {
+    if (active) {
+      const interval = setInterval(() => setTime(time - 1), 1000);
+    return () => clearInterval(interval)
+    }
+  }, [time, active])
 
   function handleClickSession(e) {
     if (e.target.id === "session-decrement") {
       session > 1 ? setSession(session - 1) : setSession(session)
     } else {
-      setSession(session + 1)
+      session < 60 ? setSession(session + 1) : setSession(session)
     }
   }
 
@@ -23,16 +32,26 @@ function App() {
     if (e.target.id === "break-decrement") {
       sbreak > 1 ? setSbreak(sbreak - 1) : setSbreak(sbreak)
     } else {
-      setSbreak(sbreak + 1)
+      sbreak < 60 ? setSbreak(sbreak + 1) : setSbreak(sbreak)
     }
   }
 
+  function handleReset() {
+    setActive(false)
+    setSession(25)
+    setSbreak(5)
+    setTime(1500)
+  }
+
+  function handleStartStop() {
+    setActive(!active)
+  }
 
   return (
     <Container fluid="lg" className="d-flex flex-column vh-100 justify-content-around justify-content-md-start">
       <Row className="bg-success flex-fill">
         <Col className="m-auto">
-          <CountdownClock />
+          <CountdownClock time={time} handleReset={handleReset} handleStartStop={handleStartStop}/>
         </Col>
       </Row>
       <Row className="bg-white flex-fill">
--- src/components/CountdownClock.js
+++ src/components/CountdownClock.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import {Container, Row, Button } from 'react-bootstrap'
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
-function CountdownClock() {
+function CountdownClock(props) {
+
+  const formatNumber = (num) => (num < 10) ? `0${num}` : num;
+
+  var minutes = formatNumber(Math.floor(props.time/60));
+  var seconds = formatNumber(props.time%60);
+
+  var displayTime = `${minutes}:${seconds}`
+
   return (
     <Container fluid className="p-1 p-lg-5 text-center text-white d-flex flex-column justify-content-around">
       <Row className="m-auto">
         <h3 id="timer-label" className="display-3 text-center">Work</h3>
       </Row>
       <Row className="m-auto">
-        <h1 id="time-left" className="display-1 font-weight-bold">25:00</h1>
+        <h1 id="time-left" className="display-1 font-weight-bold">{displayTime}</h1>
       </Row>
       <Row className="justify-content-around">
-        <Button id="start_stop" className="btn-success btn-lg btn-outline-light font-weight-bold">play/pause</Button>
-        <Button id="reset" className="btn-success btn-lg btn-outline-light font-weight-bold">reset</Button>
+        <Button id="start_stop" className="btn-success btn-lg btn-outline-light font-weight-bold" onClick={props.handleStartStop}>play/pause</Button>
+        <Button id="reset" className="btn-success btn-lg btn-outline-light font-weight-bold" onClick={props.handleReset}>reset</Button>
       </Row>
     </Container>
   )
